eafe5c4f4e0e5d5abcc7d4b08a42cb94364ea3fe
jarnold92 <37152324+jarnold92@users.noreply.github.com>
2018-03-12 06:51:44 -0700

    Update requirements.sh
    added pip3 statements for modules needed

--- requirements.sh
+++ requirements.sh
@@ -3,8 +3,9 @@ set -e
 
 ##### Install specific package versions with pip #####
 
-# pip3 install pandas==0.22.0
-# pip3 install matplotlib==2.1.2
+pip3 install pandas==0.22.0
+pip3 install matplotlib==2.1.2
+pip3 install seaborn==0.8.1
 # pip3 install scikit-learn==0.19.1
 
 
